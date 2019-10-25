b653e967939e3869c722854b3063f16fc616b965
George <UP843814@myport.ac.uk>
2019-10-25 12:05:50 +0100

    Added in an Enemy Attack Script.
    Currently not working.

---
+++ Assets/Scrips/EnemyAttack.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float damageAmount = 5;
+    public GameObject Player;
+    PlayerHealth pHealth;
+    bool inAttackRange;
+
+    void OnCollisionEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Colliding!");
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == Player)
+        {
+            inAttackRange = false;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
