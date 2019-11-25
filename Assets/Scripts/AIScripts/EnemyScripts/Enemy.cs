eb3fa2b5f630bb01e435ee2a0a63cb07b2f62897
George-Honeywell <UP843814@myport.ac.uk>
2019-11-25 20:13:03 +0000

    Code Clean up.

--- Assets/Scripts/AIScripts/EnemyScripts/Enemy.cs
+++ Assets/Scripts/AIScripts/EnemyScripts/Enemy.cs
@@ -6,33 +6,16 @@ public class Enemy : MonoBehaviour
 {
     public State<Enemy> m_State;
     public GameObject Player;
-    //public float speed;
-    //public int randomPos;
-    //public float waitTime;
-    //public float startWaitTime;
-    //public Transform[] moveToPos;
-
-    //<SUMMARY>
-    //Steering Behavior Variables
-    //</SUMMARY>
-    //public Vector3 velocity;
-    //public float mass = 1;
-    //public float maxSpeed = 1;
-    //public float maxForce = 1;
-    //public float maxTurnRate = 1.0f;
     public Agent agent;
 
     void Start()
     {
         agent = GetComponent<Agent>();
         m_State = new Patrol();
-        //waitTime = startWaitTime;
-        //randomPos = Random.Range(0, moveToPos.Length);
     }
 
     void Update()
     {
         m_State.Execute(this);
-
     }
 }
