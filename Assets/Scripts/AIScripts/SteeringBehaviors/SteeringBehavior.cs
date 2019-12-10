3d50263dc153b70fbe0f055eaed2d5cb9b05143b
George-Honeywell <UP843814@myport.ac.uk>
2019-12-10 21:04:58 +0000

    Added in the Arrive & Path Following Behavior

--- Assets/Scripts/AIScripts/SteeringBehaviors/SteeringBehavior.cs
+++ Assets/Scripts/AIScripts/SteeringBehaviors/SteeringBehavior.cs
@@ -10,15 +10,21 @@ public class SteeringBehavior : MonoBehaviour
 
     [Header("Seek")]
     //Seek
+    Vector3 seekOnTargetPos;
     bool isSeekOn = false;
     bool isPathOn = false;
-    Vector3 seekOnTargetPos;
     float seekOnStopDistance;
+
+    [Header("Arrive")]
+    public Vector3 toTarget;
+    public Vector3 distanceToTarget;
+    Vector3 targetPosition;
+    bool isArriveOn;
 
     [Header("Wander")]
     //Wander -> not needed?
-    bool isWanderOn = false;
     public float wanderRadius = 10.0f;
+    bool isWanderOn = false;
     public float wanderDistance = 10.0f;
     public float wanderJitter = 1.0f;
     public Vector3 wanderTarget = Vector3.zero;
@@ -27,29 +33,17 @@ public class SteeringBehavior : MonoBehaviour
 
     //Obstalce Avoidance Variables
     [Header("Obstalce Avoidance")]
-    bool isObstalceOn = true;
     public LayerMask layerMask;
-    public float boundingSphereRadius = 1.0f;
-    public float obstalceDistance = 10.0f;
-    [Range(0, 90)]
-    public float floorAngle = 45.0f;
-    public float forceTimer = 0;
-    public Vector3 oldSteeringForce = Vector3.zero;
+    bool isObstalceOn = true;
     public Vector3 desiredVelocity = Vector3.zero;
-    
-    public Vector3 Centre;
-    public float maxDistance = 25.0f;
-
-    //New Obstacle Avoidance Variables.
     public Vector3 steeringForce = Vector3.zero;
 
     // ~ Does this have to be called in START or AWAKE?
-    public GameObject[] objects;
+    GameObject[] objects;
     public ProjectedCube projectedCube;
     GameObject obstacleClosestGameObject;
     public float obstacleBoxSize;
 
-    //public List<GameObject> obstacles = GameObject.FindGameObjectsWithTag("Obstacles");
 
 
     void Start()
@@ -100,6 +94,11 @@ public class SteeringBehavior : MonoBehaviour
         {
             velocitySum += ObstacleAvoidance();
         }
+
+        if (isArriveOn)
+        {
+            velocitySum += Arrive(targetPosition);
+        }
         return velocitySum;
     }
 
@@ -112,6 +111,23 @@ public class SteeringBehavior : MonoBehaviour
         Debug.Log("Seek ON");
         Vector3 desiredVelocity = (targetPos - transform.position).normalized * agent.maxSpeed;
         return (desiredVelocity - agent.velocity);
+    }
+
+    Vector3 Arrive(Vector3 targetPosition)
+    {
+        Vector3 toTarget = targetPosition - agent.transform.position;
+        float distance = toTarget.magnitude;
+        float slowingDistance;
+
+        if(distance > 0)
+        {
+            slowingDistance = 0.5f;
+            agent.speed = distance / slowingDistance;
+            Mathf.Clamp(agent.speed, 0.0f, 1.0f);
+            desiredVelocity = toTarget.normalized * agent.speed / distance;
+        }
+
+        return desiredVelocity - agent.velocity;
     }
 
     Vector3 Flee(Vector3 targetPos)
@@ -142,6 +158,11 @@ public class SteeringBehavior : MonoBehaviour
         isSeekOn = true;
         seekOnTargetPos = targetPos;
         seekOnStopDistance = stoppingDistance;
+    }
+
+    public void ArriveOn()
+    {
+        isArriveOn = true;
     }
 
     public void WanderOn()
@@ -175,135 +196,27 @@ public class SteeringBehavior : MonoBehaviour
         agent.velocity = Vector3.zero;
     }
 
-    //Vector3 PathFollowing()
-    //{
-    //    //If the agent is close enough to paths.currentWayPoint
-    //    if ((transform.position - path[currentWayPoint]).magnitude < 1.5)
-    //    {
-    //        //paths.setnextwaypoint
-    //        currentWayPoint++;
-    //    }
+    Vector3 PathFollowing()
+    {
+        //If the agent is close enough to paths.currentWayPoint
+        if ((transform.position - path[currentWayPoint]).magnitude < 1.5)
+        {
+            //paths.setnextwaypoint
+            currentWayPoint++;
+        }
 
-    //    //If this isn't the last way point in the list
-    //    if (path.Length <= currentWayPoint)
-    //    {
-    //        //Return Seek(Paths.CurrentWayPoint)
-    //        return Seek(path[currentWayPoint]);
-    //    }
-    //    else
-    //    {
-    //        return;
-    //    }
-    //Else this is the last waypoint
-    //else
-    //{
-    //    //Return Arrive(Paths.CurrentWayPoint)
-    //    return seekOnTargetPos;
-    //}
-    //}
-
-
-    //Steering Behavior - Obstalce Avoidance
-    #region Obstacle Avoidance
-    //Vector3 ObstacleAvoidance()
-    //{
-    //    //Cast a ray from the centre of the agent, in it's forward direction
-    //    Ray ray = new Ray(transform.position, transform.forward);
-
-    //    //Name a raycastHit
-    //    RaycastHit hitInfo;
-
-    //    //Set avoidanceForce to ZERO for all axis
-    //    Vector3 avoidanceForce = Vector3.zero;
-
-    //    //Calculate the 'Avoidance Force'
-    //    if (Physics.BoxCast(transform.position, new Vector3(2.5f, 2.5f, 20.0f), transform.forward, out hitInfo, transform.rotation, maxDistance))
-    //    //if(Physics.SphereCast(ray, boundingSphereRadius, out hitInfo, obstalceDistance, layerMask))
-    //    {
-    //        if (Vector3.Angle(hitInfo.normal, transform.up) > floorAngle)
-    //        {
-    //            //Reflect the Vector
-    //            avoidanceForce = Vector3.Reflect(agent.velocity, hitInfo.normal);
-
-    //            //Calculate the dot product between the Force and Velocity
-    //            if (Vector3.Dot(avoidanceForce, agent.velocity) < 0)
-    //            {
-    //                //Transform Right
-    //                avoidanceForce = transform.right;
-    //                Debug.Log(hitInfo);
-    //            }
-    //        }
-    //    }
-
-    //    if (avoidanceForce != Vector3.zero)
-    //    {
-    //        //Calculate desired velocity
-    //        desiredVelocity = (avoidanceForce).normalized * agent.maxSpeed;
-    //        desiredVelocity = avoidanceForce * agent.maxSpeed;
-
-    //        //Calculate the steering Force
-    //        steeringForce = desiredVelocity - agent.velocity;
-    //        oldSteeringForce = steeringForce;
-    //    }
-    //    else
-    //    {
-    //        steeringForce = Vector3.zero;
-    //    }
-    //    return steeringForce;
-    //}
-    #endregion
-
-
-    //Vector3 ObstacleAvoidance()
-    //{
-    //    //Variables
-    //    //GameObject[] obstalces = GameObject.FindGameObjectsWithTag("Obstacles");
-    //    float forceMultipler;
-    //    float boxLength = 20.0f;
-    //    float obstacleRadius = 5.0f;
-    //    Vector3 obstaclePos = new Vector3();
-
-    //    //Project a detection box in front of the agent.
-    //    RaycastHit hitInfo;
-    //    Physics.BoxCast(transform.position, new Vector3(2.5f, 2.5f, 20.0f), transform.forward, out hitInfo, transform.rotation, maxDistance);
-
-    //    //Iterate through all "tagged obstacle" and convert them to local space (relative to the vechicle's transform)       
-    //    //int objectCount = objects.Length;
-
-    //    foreach (GameObject _object in objects)
-    //    {
-    //        //Convert tagged obstacle from WORLD to LOCAL space
-    //        obstaclePos = transform.InverseTransformPoint(obstaclePos);
-            
-    //    }
-
-    //    //Check if objects intersect with detection box
-
-
-
-    //    //Find all Intsersection Points
-    //    // ~Use the closest intersection point to the agent
-    //    // ~Find what obstalce the intersection point belongs to and use that as the closest obstalce
-
-    //    //Where do I get the obstacle.LocalPosition.X/Y from?
-    //    //Where do I get the obstacle.radius from?
-
-    //    //Now if we have a CLOEST OBSTACLE
-    //    //Assuming this would be in an IF statement checking if an obstacle is close?
-    //    forceMultipler = 1 + (boxLength - obstaclePos.x) / boxLength;
-    //    // ~ The further away the obstacle is, the smaller the ForceMultipler becomes.
-
-
-    //    steeringForce.y = (obstacleRadius - obstaclePos.y) * forceMultipler;
-    //    // ~ Get a Y direction that is perpendicular to the obstacle relative to the agent's location
-
-    //    //This was all done in LOCAL SPACE
-    //    transform.InverseTransformPoint(steeringForce);
-    //    // ~ Convert SteeringForce(which is a direction) back to WORLD space and return it(matrices again)
-
-
-    //    return steeringForce;
-    //}
+        //If this isn't the last way point in the list
+        if (path.Length <= currentWayPoint)
+        {
+            //Return Seek(Paths.CurrentWayPoint)
+            return Seek(path[currentWayPoint]);
+        }
+        else
+        {
+            //???
+            return Arrive(path[currentWayPoint]);
+        }
+    }
 
     Vector3 ObstacleAvoidance()
     {
@@ -390,6 +303,7 @@ public class SteeringBehavior : MonoBehaviour
         //Gizmos.DrawLine(transform.position, transform.position + transform.forward * obstalceDistance);
         //Gizmos.matrix = transform.localToWorldMatrix;
         //Gizmos.DrawWireCube(transform.forward, new Vector3(2.5f, 2.5f, 20.0f));
+
 
 
 
